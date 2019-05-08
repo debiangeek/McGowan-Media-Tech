c9c321082196ea8389c942566307b769fe0c2927
debiangeek <45010353+debiangeek@users.noreply.github.com>
2019-05-08 10:35:03 -0500

    Refactored projects section to be DRY

--- mcgowan-media-tech/src/components/Projects.js
+++ mcgowan-media-tech/src/components/Projects.js
@@ -1,56 +1,42 @@
 import React, { Component } from "react";
 
 import "../stylesheets/Projects.css";
+import { projectList } from "./projectsConst";
 
 export default class Projects extends Component {
   render() {
     return (
       <div id="projects-container">
         <h2 className="project-text">Projects</h2>
-        <div className="project-flex">
-          <div className="project-box">
-            <p className="project-text">
-              A simple GitHub Markdown previewer, built with React.
-            </p>
-            <a
-              className="project-link"
-              href="https://markdown.mcgowanmedia.tech"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Project Site
-            </a>
-            <a
-              className="project-link"
-              href="https://github.com/debiangeek/markdown-previewer"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Source Code
-            </a>
-          </div>
-          <div className="project-box">
-            <p className="project-text">
-              Random quote generator, built with React.
-            </p>
-            <a
-              className="project-link"
-              href="https://quotes.mcgowanmedia.tech"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Project Site
-            </a>
-            <a
-              className="project-link"
-              href="https://github.com/debiangeek/Random-Quote-Generator"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Source Code
-            </a>
-          </div>
-        </div>
+        {projectList.map(pair => {
+          return (
+            <div className="project-flex">
+              {pair.map(item => {
+                return (
+                  <div className="project-box">
+                    <p className="project-text">{item.description}</p>
+                    <a
+                      className="project-link"
+                      href={item.live}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Project Site
+                    </a>
+                    <a
+                      className="project-link"
+                      href={item.source}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Source Code
+                    </a>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
     );
   }
---
+++ mcgowan-media-tech/src/components/projectsConst.js
@@ -0,0 +1,22 @@
+export const projectList = [[
+    {
+        description: "A simple GitHub Markdown previewer, built with React.",
+        live: "https://markdown.mcgowanmedia.tech",
+        source: "https://github.com/debiangeek/markdown-previewer"
+    },
+    {
+        description: "Random quote generator, built with React.",
+        live: "https://quotes.mcgowanmedia.tech",
+        source: "https://github.com/debiangeek/Random-Quote-Generator"
+    }],
+    [{
+        description: "A drum machine, built with React.",
+        live: "https://drumbox.mcgowanmedia.tech",
+        source: "https://github.com/debiangeek/music-machine"
+    },
+    {
+        description: "A simple calculator, built with React.",
+        live: "https://calc.mcgowanmedia.tech",
+        source: "https://github.com/debiangeek/calculator"
+    }],
+];
